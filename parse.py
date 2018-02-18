31a92213e1df8dfa7a5d9792be25905d9c151924
qwerty432 <dimichkit@ukr.net>
2018-02-18 22:54:56 +0200

    improved output of time

--- parse.py
+++ parse.py
@@ -3,6 +3,10 @@ import requests
 from selenium import  webdriver
 
 
+#dict for endings (1 day, 3 days etc.)
+ending_s_dict = {'days': 's', 'hours': 's', 'mins': 's'}
+
+
 #function to parse remaining time before next match
 def parse_time() :
     url = "https://www.fcbarcelona.com/"
@@ -22,8 +26,18 @@ def parse_time() :
     minutes_left = time_left.find('span', id = 'mins').text
 
 
-    time = "Time to next match: {} days, {} hours {} minutes".\
-        format(days_left, hours_left, minutes_left)
+    if int(days_left) == 1 :
+        ending_s_dict['days'] = ''
+    elif int(hours_left) == 1 :
+        ending_s_dict['hours'] = ''
+    elif int(minutes_left) == 1 :
+        ending_s_dict['mins'] = ''
+
+    print(ending_s_dict['days'])
+
+    time = "Time to next match: {} day{}, {} hour{} {} minute{}".\
+        format(days_left, ending_s_dict['days'], hours_left, \
+                ending_s_dict['hours'], minutes_left, ending_s_dict['mins'])
     print(time)
 
     return time
