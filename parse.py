5ec2a1ad24b37a1219dbab49d2d8b15435a0075c
qwerty432 <dimichkit@ukr.net>
2018-02-19 23:30:01 +0200

    added function for downloading image

--- parse.py
+++ parse.py
@@ -3,11 +3,20 @@ import requests
 from selenium import  webdriver
 
 
+#download image by url to 'image' folder
+def download_image(url, name) :
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open('images/' + name, 'wb') as f:
+            f.write(r.content)
+
+
+
 #dict for endings (1 day, 3 days etc.)
-ending_s_dict = {'days': 's', 'hours': 's', 'mins': 's'}
+endings = {'days': 's', 'hours': 's', 'mins': 's'}
 
 
-#function to parse remaining time before next match
+#parse remaining time before next match
 def parse_time() :
     url = "https://www.fcbarcelona.com/"
     # page = requests.get(url)
@@ -27,17 +36,17 @@ def parse_time() :
 
 
     if int(days_left) == 1 :
-        ending_s_dict['days'] = ''
+        endings['days'] = ''
     elif int(hours_left) == 1 :
-        ending_s_dict['hours'] = ''
+        endings['hours'] = ''
     elif int(minutes_left) == 1 :
-        ending_s_dict['mins'] = ''
+        endings['mins'] = ''
 
-    print(ending_s_dict['days'])
+    print(endings['days'])
 
     time = "Time to next match: {} day{}, {} hour{} {} minute{}".\
-        format(days_left, ending_s_dict['days'], hours_left, \
-                ending_s_dict['hours'], minutes_left, ending_s_dict['mins'])
+        format(days_left, endings['days'], hours_left, \
+                endings['hours'], minutes_left, endings['mins'])
     print(time)
 
     return time
