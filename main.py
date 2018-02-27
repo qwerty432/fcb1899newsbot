aa8865819f667f0084dd0e6864973a417f99c119
qwerty432 <dimichkit@ukr.net>
2018-02-27 22:58:15 +0200

    refactored and added function for clearing image folder

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 from flask import Flask, request
 import telebot, json
-from constants import token, secret
+from constants import token, secret, my_chatid
+import parse
 from datetime import datetime
+from time import sleep
 
 
 # def write_json(data, filename='answer.json'):
 #     with open(filename, 'w') as f:
 #         json.dump(data, f, indent=2, ensure_ascii=False)
 
-global ten_urls
+
+ten_urls = []
+
+
+def write_logs(message, username, chat_id, time) :
+    with open('logs.txt', 'a') as file :
+        file.write('\n' + message + '\n' + username  + '\n' + str(chat_id) +\
+                     '\n' + str(time) + '\n')
+
+        if chat_id != my_chatid :
+            bot.send_message(my_chatid, "{} ({}) uses bot"\
+                             .format(username, chat_id))
 
 
 bot = telebot.TeleBot(token, threaded=False)
 app = Flask(__name__)
 
 bot.remove_webhook()
-bot.set_webhook(url="https://dimakit.pythonanywhere.com/{}".format(secret))
+# bot.set_webhook(url="https://dimakit.pythonanywhere.com/{}".format(secret))
 
-
-
-#function for writing logs of every message
-def write_logs(message, username, chat_id, time) :
-    with open('logs.txt', 'a') as file :
-        file.write('\n' + message + '\n' + username  + '\n' + str(chat_id) +\
-                     '\n' + str(time) + '\n')
 
 
 @app.route('/{}'.format(secret), methods=["POST"])
@@ -33,43 +39,55 @@ def webhook():
     return "ok", 200
 
 
-#handler for '/start' command
+#handler for /start command
 @bot.message_handler(commands=['start'])
 def start_command(message) :
-    write_logs(message.text, message.from_user.username, message.chat.id, datetime.now())
-    print(message)
+    write_logs(message.text, 
+               message.from_user.username, 
+               message.chat.id, 
+               datetime.now())
     bot.reply_to(message, "Welcome! Here you can find news about Barcelona :)")
 
 
-#handler for '/help' command
+#handler for /help command
 @bot.message_handler(commands=['help'])
 def help_command(message) :
-    write_logs(message.text, message.from_user.username, message.chat.id, datetime.now())
-    bot.reply_to(message, "Памагити")
+    write_logs(message.text, 
+               message.from_user.username, 
+               message.chat.id, 
+               datetime.now())
+    bot.reply_to(message, "Heeelp!!")
 
 
 #handler for /next command
 @bot.message_handler(commands=['next'])
-def next_command(message) :
-    write_logs(message.text, message.from_user.username, message.chat.id, datetime.now())
+def next_command(message) :    
+    write_logs(message.text, 
+               message.from_user.username, 
+               message.chat.id, 
+               datetime.now())
     bot.send_message(message.chat.id, parse.parse_info())
 
 
-
-#handler for '/time' command
+#handler for /time command
 @bot.message_handler(commands=['time'])
 def time_command(message) :
-    write_logs(message.text, message.from_user.username, message.chat.id, datetime.now())
+    write_logs(message.text, 
+               message.from_user.username, 
+               message.chat.id, 
+               datetime.now())
     bot.send_message(message.chat.id, "Time to next match: {}".format(parse.parse_time()))
 
 
-
-#handler for '/news' command
+#handler for /newa command
 @bot.message_handler(commands=['news'])
 def news_command(message) :
     global ten_urls
     ten_urls = []
-    write_logs(message.text, message.from_user.username, message.chat.id, datetime.now())
+    write_logs(message.text, 
+               message.from_user.username, 
+               message.chat.id, 
+               datetime.now())
 
     keyboard = telebot.types.InlineKeyboardMarkup()
     news = parse.parse_latest_news()
@@ -85,13 +103,14 @@ def news_command(message) :
     bot.send_message(message.chat.id, "Last news:", reply_markup=keyboard)
 
 
-
-
+#handler for text content
 @bot.message_handler(content_types=["text"])
 def any_msg(message):
-    write_logs(message.text, message.from_user.username, message.chat.id, datetime.now())
+    write_logs(message.text, 
+               message.from_user.username, 
+               message.chat.id, 
+               datetime.now())
     bot.send_message(message.chat.id, "You said: {}".format(message.text))
-
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -104,12 +123,12 @@ def callback_inline(call):
                 if(len(instant_view)) == 2 :
                     bot.send_message(call.message.chat.id, instant_view[0])
                     sleep(1)
-                    bot.send_message(call.message.chat.id,  instant_view[1])
+                    bot.send_message(call.message.chat.id, instant_view[1])
                 else :
                     bot.send_message(call.message.chat.id, instant_view)         
 
 
-
+bot.polling(none_stop=False)
 
 
 if __name__ == '__main__':
--- parse.py
+++ parse.py
@@ -1,59 +1,25 @@
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
+from datetime import datetime, date
+import os
 import requests
-from selenium import  webdriver
-from constants import teletoken
+from bs4 import BeautifulSoup
 from telegraph import Telegraph
 from telegraph.exceptions import TelegraphException
-from datetime import datetime, date
-
-
-months = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 
-            'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10,
-            'November': 11, 'December': 12,}
-
+from constants import teletoken, BASE_DIR
 
 #dict for endings (1 day, 3 days etc.)
 endings = {'days': 's', 'hours': 's', 'mins': 's'}
-
-
-
-def parse_next_match(parameter) :
-
-    url = "http://football.ua/club/51-barcelona.html"
-    page = requests.get(url)
-    html = page.text
-
-    soup = BeautifulSoup(html, 'lxml')
-
-
-    next_matches = soup.find_all('table', class_='feed-table')[1].find_all('tr')
-    next_match_where = next_matches[0].find('p').text.replace(' ', '').replace('\r', '').split('\n')[1:-1]
-    next_match_date = next_match_where[0]
-    next_match_tournament = next_match_where[1]
-    next_match_stage = next_match_where[2]
-
-
-    next_match_time = next_matches[1].find_all('td')[0].text
-    next_match_home = next_matches[1].find_all('td')[1].text.replace(' ', '').replace('\n', '')
-    next_match_guest = next_matches[1].find_all('td')[3].text.replace(' ', '').replace('\n', '')
-
-    if parameter == 'time' :
-        return next_match_date, next_match_time
-    elif parameter == 'info' :
-        return next_match_home, next_match_guest, next_match_tournament, \
-                next_match_stage, next_match_date, next_match_time
-
 
 
 #download image by url to 'images' folder
 def download_image(url, name) :
     r = requests.get(url, stream=True)
     if r.status_code == 200:
-        with open('images/' + name, 'wb') as f:
+        with open(BASE_DIR + '/images/' + name, 'wb') as f:
             f.write(r.content)
 
-#makes new path of image for inserting to Instant View
+
+#makes new path of image appropriate for telegraph
 def make_path(path):
     with open(path, 'rb') as f:
         new_path = requests.post(
@@ -63,20 +29,66 @@ def make_path(path):
     return new_path[0]['src']
 
 
-#prepares image
-def prepare_image(p, image_url) :
+#prepares image for Instant View
+def prepare_image(image_url) :
     image_name = image_url.split('/')[-1]
 
     download_image(image_url, image_name)
 
-    p = str(p)
     new_path = make_path('images/{}'.format(image_name))
-    p = '<img src="{}"/>'.format(new_path)
+    p = '<img src="{}"/>'.format(new_path)  #creates paragraph tag with image
     return p
 
 
+def clear_images() :
+    os.chdir(BASE_DIR + '/images/')
+    all_files = os.listdir()
+    images = [os.remove(f) for f in all_files \
+                                            if f.endswith('.jpg')\
+                                            or f.endswith('.png')]
+    os.chdir(BASE_DIR)
 
-#parse info about next match
+
+#parses all information about next match
+def parse_next_match(parameter) :
+    url = "http://football.ua/club/51-barcelona.html"
+    page = requests.get(url)
+    html = page.text
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    #scrapes next matches table
+    next_matches = soup.find_all('table', class_='feed-table')[1]\
+                       .find_all('tr') 
+    
+    #scrapes info about when and in which tournament next match will be
+    next_match_where = next_matches[0].find('p').text\
+                                      .replace(' ', '')\
+                                      .replace('\r', '')\
+                                      .split('\n')[1:-1]
+
+    next_match_date = next_match_where[0]
+    next_match_tournament = next_match_where[1]
+    next_match_stage = next_match_where[2]
+
+    #scrapes info about home and guest teams and time of the match
+    next_match_time = next_matches[1].find_all('td')[0].text
+    next_match_home = next_matches[1].find_all('td')[1].text\
+                                     .replace(' ', '')\
+                                     .replace('\n', '')
+
+    next_match_guest = next_matches[1].find_all('td')[3].text\
+                                      .replace(' ', '')\
+                                      .replace('\n', '')
+
+    if parameter == 'time' :
+        return next_match_date, next_match_time
+    elif parameter == 'info' :
+        return next_match_home, next_match_guest, next_match_tournament, \
+                next_match_stage, next_match_date, next_match_time
+
+
+#parses general information about next match
 def parse_info() :
     info = parse_next_match('info')
 
@@ -87,16 +99,16 @@ def parse_info() :
     stage = info[3]
 
     date = info[4]
-
     time = info[5]
 
-    return "📌 Следующий матч:\n⚽{} — {}\n🏆{}, {}\n📅{}, {}".format(home, guest,\
-                                tournament, stage, date, time)
+    return "📌 Следующий матч:\n⚽ {} — {}\n🏆 {}, {}\n📅 {}, {}"\
+                                .format(home, guest, tournament, \
+                                        stage, date, time)
 
 
 #parse remaining time before next match
 def parse_time() :
-    date, time = parse_next_match('time')
+    date, time = parse_next_match('time')   #get time and date of next match
     day = int(date.split('.')[0])
     month = int(date.split('.')[1])
     year = int(date.split('.')[2])
@@ -104,11 +116,10 @@ def parse_time() :
     hours = int(time.split(':')[0])
     minutes = int(time.split(':')[1])
 
-    now = datetime.now()
+    now = datetime.now()    #exact time when request made
     match_date = datetime(year, month, day, hours, minutes, 0)
 
-    time_left = match_date - now
-
+    time_left = match_date - now    #calculate remaining time
 
     # if int(days_left) == 1 :
     #     endings['days'] = ''
@@ -137,123 +148,106 @@ def parse_article(url, too_big=False) :
     soup = BeautifulSoup(html, 'lxml')
     article = soup.find_all('article')[0]
     
-    title = article.find_all('h1')[0].text
-    paragraphs = article.find_all('p')[1:]
-
+    title = article.find_all('h1')[0].text  #title of article
+    paragraphs = article.find_all('p')[1:]  #all useful text from article
 
     for p in paragraphs :
         if 'img' in str(p) :
-            image_url = p.find('img')['src']
-            p = prepare_image(p, image_url)
+            image_url = p.find('img')['src']    #get image's url
+            p = prepare_image(image_url)    #create image path appr. for Telegraph
 
-        if('span' not in str(p)) :
+        if('span' not in str(p)) :  #'span' tag is not allowed in telegraph
             content += str(p)     
 
-
-        if 'class="intro"' in str(p) :
-            image_url = article.find('div', class_='article-photo').\
-                                            find('img')['src']
-            p = prepare_image(p, image_url)
+        if 'class="intro"' in str(p) :  #get article photo
+            image_url = article.find('div', class_='article-photo')\
+                               .find('img')['src']
+            p = prepare_image(image_url)
             content += str(p)
-
     
     #if article is too big we split it into two different Instant Views
     if(too_big) :
-        content_list = content.split('<p')
-        print(len(content_list))
+        content_list = content.split('<p')  #split all paragraphs
+        middle = int(len(content_list) / 2) #middle of all paragraphs
 
-
-        middle = int(len(content_list) / 2)
-
-        content1 = '<p'.join(content_list[:middle])
+        content1 = '<p'.join(content_list[:middle]) #first page
         
         content_list[middle] = '<p' + content_list[middle]
-        content2 = '<p'.join(content_list[middle:])
-
+        content2 = '<p'.join(content_list[middle:]) #second page
 
         print(content2)
 
-        titles = [title + '. Part1', title + '. Part2']
+        titles = [title + '. Part1', title + '. Part2'] #titles for 2 pages
         content = [content1, content2]
 
         return titles, content
 
-
-
-
     return title, content
 
-
-
+#function for creating Instant View
 def create_instant_view(url) :
     telegraph = Telegraph(teletoken)
     too_big = False
     
     title, content = parse_article(url)
+    clear_images()
 
     try :
         response = telegraph.create_page(title=title, html_content=content)
-        return response['url']
+        return response['url']  #url of created telegraph page
 
-    except TelegraphException :
+    except TelegraphException : #if article is too big
         print("Oh no, something went wrong.")
         titles, contents = parse_article(url, too_big=True)    
 
-        response1 = telegraph.create_page(title=titles[0], html_content=contents[0])
-        response2 = telegraph.create_page(title=titles[1], html_content=contents[1])
-
+        response1 = telegraph.create_page(title=titles[0], 
+                                          html_content=contents[0])
+        response2 = telegraph.create_page(title=titles[1], 
+                                          html_content=contents[1])
+        #urls of created telegraph pages
         return [response1['url'], response2['url']]
 
-
-
-
-
+#parse 10 latest news about Barca
 def parse_latest_news() :
     all_news = []
     page_num = 1
     titles = []
     urls = []
 
+    #keywords of which searching Barca news
     keywords = ['Барселона', 'Вальверде', 'Месси', 'Неймар', 'Суарес', \
                 'Иньеста', 'Букскетс', 'Дембеле', 'Коутиньо', 'Паулиньо', \
                 'Тер Стеген', 'Альба', 'Пике', 'Умтити', 'Роберто', 'Гомеш', \
                 'Бартомеу', 'Каталония', 'Камп Ноу', 'Силессен', 'Мина', \
                 'Видаль', 'Динь', 'Семеду', 'Вермален', 'Денис Суарес',\
-                'Денис', 'Ракитич', 'Алькасер', 'Пако', 'Арнаис', 'Анонс', 'Итоги']
+                'Денис', 'Ракитич', 'Алькасер', 'Пако', 'Арнаис', 'Анонс', \
+                'Итоги']
 
     while(len(urls) < 10) :
-        main_url = 'http://football.ua/news/archive/spain/page{}.html'.format(page_num)
-
-
+        main_url = 'http://football.ua/news/archive/spain/page{}.html'\
+                                                        .format(page_num)
         page = requests.get(main_url)
         html = page.text
-
-
 
         soup = BeautifulSoup(html, 'lxml')
 
         news = soup.find('section', class_='news-archive')
-
         other_news = news.find('ul', class_='archive-list').find_all('li')
         
         for article in other_news :
             flag = False
             title = article.find('h4').find('a')
             url = title['href']
-
             short_descr = article.find('a', class_='intro-text')
             
             for keyword in keywords :
-                if keyword in str(title)and not flag and len(urls) < 10:
+                if keyword in str(title) and not flag and len(urls) < 10:
                     urls.append(url)
                     titles.append(title.text)
-
                     flag = True
-
         page_num += 1
 
     all_news.append(titles)
     all_news.append(urls)
 
     return all_news
-
