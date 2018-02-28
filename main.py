1772ca5db8c9edb0267f56c190000fd48961a869
qwerty432 <dimichkit@ukr.net>
2018-02-28 16:12:35 +0200

    replaced 'range(len(smth))' with 'enumerate(smth)'

--- main.py
+++ main.py
@@ -42,40 +42,32 @@ def webhook():
 #handler for /start command
 @bot.message_handler(commands=['start'])
 def start_command(message) :
-    write_logs(message.text, 
-               message.from_user.username, 
-               message.chat.id, 
-               datetime.now())
+    write_logs(message.text, message.from_user.username, 
+               message.chat.id, datetime.now())
     bot.reply_to(message, "Welcome! Here you can find news about Barcelona :)")
 
 
 #handler for /help command
 @bot.message_handler(commands=['help'])
 def help_command(message) :
-    write_logs(message.text, 
-               message.from_user.username, 
-               message.chat.id, 
-               datetime.now())
+    write_logs(message.text, message.from_user.username, 
+               message.chat.id, datetime.now())
     bot.reply_to(message, "Heeelp!!")
 
 
 #handler for /next command
 @bot.message_handler(commands=['next'])
 def next_command(message) :    
-    write_logs(message.text, 
-               message.from_user.username, 
-               message.chat.id, 
-               datetime.now())
+    write_logs(message.text, message.from_user.username, 
+               message.chat.id, datetime.now())
     bot.send_message(message.chat.id, parse.parse_info())
 
 
 #handler for /time command
 @bot.message_handler(commands=['time'])
 def time_command(message) :
-    write_logs(message.text, 
-               message.from_user.username, 
-               message.chat.id, 
-               datetime.now())
+    write_logs(message.text, message.from_user.username, 
+               message.chat.id, datetime.now())
     bot.send_message(message.chat.id, "Time to next match: {}".format(parse.parse_time()))
 
 
@@ -84,18 +76,19 @@ def time_command(message) :
 def news_command(message) :
     global ten_urls
     ten_urls = []
-    write_logs(message.text, 
-               message.from_user.username, 
-               message.chat.id, 
-               datetime.now())
+    write_logs(message.text, message.from_user.username, 
+               message.chat.id, datetime.now())
 
     keyboard = telebot.types.InlineKeyboardMarkup()
+    callback_buttons = []
     news = parse.parse_latest_news()
     titles = news[0]
     ten_urls.extend(news[1])
-    callback_buttons = []
-    for i in range(len(titles)) :
-        callback_buttons.append(telebot.types.InlineKeyboardButton(text=titles[i], callback_data="test{}".format(i)))
+    for i, title in enumerate(titles) :
+        button = telebot.types.InlineKeyboardButton(
+                                    text=title, 
+                                    callback_data="button{}".format(i))
+        callback_buttons.append(button)
 
     for button in callback_buttons :    
         keyboard.add(button)
@@ -106,10 +99,8 @@ def news_command(message) :
 #handler for text content
 @bot.message_handler(content_types=["text"])
 def any_msg(message):
-    write_logs(message.text, 
-               message.from_user.username, 
-               message.chat.id, 
-               datetime.now())
+    write_logs(message.text, message.from_user.username, 
+               message.chat.id, datetime.now())
     bot.send_message(message.chat.id, "You said: {}".format(message.text))
 
 
@@ -117,9 +108,9 @@ def any_msg(message):
 def callback_inline(call):
     global ten_urls
     if call.message:
-        for i in range(len(ten_urls)) :
-            if call.data == "test{}".format(i):
-                instant_view = parse.create_instant_view(ten_urls[i])
+        for i, url in enumerate(ten_urls) :
+            if call.data == "button{}".format(i):
+                instant_view = parse.create_instant_view(url)
                 if(len(instant_view)) == 2 :
                     bot.send_message(call.message.chat.id, instant_view[0])
                     sleep(1)
