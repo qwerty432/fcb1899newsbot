6b9613a1d5ee0cc514784060cbbfefb6ea40159b
qwerty432 <dimichkit@ukr.net>
2018-09-10 21:55:22 +0300

    added new dictionary with links on championats

---
+++ useful_dictionaries.py
@@ -0,0 +1,7 @@
+CHAMPIONATS_DICT = {'Украина': 'https://football.ua/ukraine.html',
+                    'Англия': 'https://football.ua/england.html',
+                    'Германия': 'https://football.ua/germany.html',
+                    'Испания': 'https://football.ua/spain.html',
+                    'Италия': 'https://football.ua/italy.html',
+                    'Нидерланды': 'https://football.ua/netherlands.html'
+                    }
