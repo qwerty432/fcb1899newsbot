01f302102c37c6b5ceca67df202e23e0cf65684e
qwerty432 <dimichkit@ukr.net>
2018-09-20 21:42:14 +0300

    added languages file

---
+++ languages.py
@@ -0,0 +1,16 @@
+LANG_DICT = {
+        'ru': {
+            'hello_msg': 'Привет!',
+            'last_match_btn': 'Последний матч',
+            'next_match_btn':'Следующий матч',
+            'news_btn': 'Новости',
+            'articles_btn': 'Статьи',
+            'squad_btn': 'Состав',
+            'time_btn': 'Время',
+            'settings_btn': 'Настройки',
+            'choose_team_btn': 'Выбрать команду',
+            'return_btn': 'Назад',
+            'choose_champ_msg': 'Выберите чемпионат:',
+            'choose_team_msg': 'Выберите команду:'
+        }
+}
